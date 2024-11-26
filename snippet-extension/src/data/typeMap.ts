349864443ffe40f3c138668ae700bc34819171bb
leonlai257 <leonlai257@gmail.com>
2024-11-25 22:55:06 -0500

    updated logic for definitions

---
+++ snippet-extension/src/data/typeMap.ts
@@ -0,0 +1,8 @@
+export const scriptTypeMap = {
+  WithdrawalScriptHashes: "WithdrawalScriptHashes",
+  ScriptHash: "ScriptHash",
+  KeyHash: "PubKeyHash",
+  Address: "ScriptAddress",
+  PolicyId: "PolicyId",
+  ByteArray: "ByteString",
+};
--- snippet-extension/src/extension.ts
+++ snippet-extension/src/extension.ts
@@ -1,4 +1,5 @@
 import * as vscode from "vscode";
+import { scriptTypeMap } from "./data/typeMap";
 
 export function activate(context: vscode.ExtensionContext) {
   console.log("Cardano devkit extension activated");
@@ -22,37 +23,120 @@ export function activate(context: vscode.ExtensionContext) {
           const fs = require("fs");
           const path = fileUri[0].fsPath;
           const data = fs.readFileSync(path, "utf8");
+
           const script = JSON.parse(data);
+          const definitions = script.definitions;
 
-          Object.keys(script).forEach((key) => {
-            const title = script[key].title;
-            const fields = script[key].anyOf[0]?.fields;
+          let imports: string[] = []; // Types that needs to be imported
+          let customs: string[] = []; // Custom types that imports are not needed
+          let fullSnippet: string[] = [];
 
-            if (title) {
-              const code = [`export type ${script[key].title} = ConStr0<[`];
+          if (definitions) {
+            Object.keys(definitions).forEach((key) => {
+              const title = definitions[key]?.title;
+              const anyOf = definitions[key]?.anyOf;
 
-              if (fields) {
-                fields.forEach((field: any) => {
-                  console.log(field);
-                  switch (field.title) {
-                    case "oracle_address":
-                      code.push(`ScriptAddress, // ${field.title}: Address`);
-                      break;
-                    case "operation_key":
-                    case "stop_key":
-                      code.push(
-                        `PubKeyHash, // ${field.title}: VerificationKeyHash,`
-                      );
-                      break;
+              if (title) {
+                // Skip if already included in the blueprint
+                if (customs.includes(title)) {
+                  return;
+                }
+
+                // Add to imports if it is not a custom type and title only contains letters
+                const regex = /^[a-zA-Z]+$/;
+
+                if (!key.includes("/") && regex.test(title)) {
+                  imports.push(title);
+                } else {
+                  customs.push(title);
+                }
+
+                if (anyOf) {
+                  let types: string[] = [];
+                  let definitionSnippet: string[] = [];
+
+                  anyOf.forEach((type: any) => {
+                    if (type.title) {
+                      types.push(type.title);
+                    }
+
+                    if (type.fields) {
+                      const fields = type.fields;
+
+                      let fieldSnippet = [
+                        `export type ${type.title} = ConStr0<[`,
+                      ];
+
+                      fields.forEach((field: any) => {
+                        const ref = field.$ref.split("/");
+                        let type = ref[ref.length - 1];
+                        if (type.includes("~1")) {
+                          const split = type.split("~1");
+                          type = split[split.length - 1];
+                        }
+
+                        let mappedType = type;
+
+                        if (type.includes("Tuple")) {
+                          const tuple = type.split("Tuple$");
+                          const tupleTypes = tuple[1].split("_");
+
+                          mappedType = `Tuple<${tupleTypes.join(", ")}>, // ${
+                            field.title
+                          }`;
+                        } else {
+                          for (const key in scriptTypeMap) {
+                            if (type.includes(key)) {
+                              mappedType =
+                                scriptTypeMap[
+                                  key as keyof typeof scriptTypeMap
+                                ];
+                              break;
+                            }
+                          }
+                        }
+
+                        fieldSnippet.push(
+                          `${mappedType}, // ${field.title}: ${type}`
+                        );
+                      });
+
+                      fieldSnippet.push(`]>;`);
+                      fieldSnippet.push("");
+
+                      definitionSnippet = [
+                        ...definitionSnippet,
+                        ...fieldSnippet,
+                      ];
+                    }
+                  });
+
+                  if (anyOf.length > 1) {
+                    definitionSnippet = [
+                      `export type ${title} = ConStr0<${types.join(" | ")}>;`,
+                      "",
+                      ...definitionSnippet,
+                    ];
                   }
-                });
-              }
 
-              code.push(`]>;`);
-              const snippet = new vscode.SnippetString(code.join("\n"));
-              vscode.window.activeTextEditor?.insertSnippet(snippet);
+                  fullSnippet = [...fullSnippet, "", ...definitionSnippet];
+                }
+              }
+            });
+
+            if (imports.length > 0) {
+              fullSnippet = [
+                `import { ConStr0, ${imports.join(
+                  ", "
+                )} } from "@meshsdk/core";`,
+                "",
+                ...fullSnippet,
+              ];
             }
-          });
+
+            const snippet = new vscode.SnippetString(fullSnippet.join("\n"));
+            vscode.window.activeTextEditor?.insertSnippet(snippet);
+          }
         }
       });
     }
